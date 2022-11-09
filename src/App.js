c6a074ea0a1a0129ef99d1c8fdf356b4f07759a4
aditya vinayak <adityavinayak@adityas-MacBook-Air.local>
2022-11-10 01:17:02 +0530

    another small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         <div>adi</div>
+         <div>aditya</div>
         </a>
       </header>
     </div>
