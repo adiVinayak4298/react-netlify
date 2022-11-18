baf93b848dede77b5502f545ce019b94ed81c5af
aditya vinayak <adityavinayak@adityas-MacBook-Air.local>
2022-11-18 18:09:33 +0530

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route,BrowserRouter} from "react-router-dom"; 
+import SignUp from './signUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,12 @@ function App() {
         >
          <div>adityavinyayk429999999911111</div>
         </a>
-      </header>
+      </header> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/signup" element = {<SignUp/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
